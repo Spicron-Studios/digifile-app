56ae8929ff2f7534888b1f499be31485642fb4e3
FireHawk Studio <39730717+LordFirehawk@users.noreply.github.com>
2024-12-22 22:33:41 +0200

    Sentry Installed

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://07ace51ce62ca5846a7183e78d6fc1b9@o4508513589264384.ingest.de.sentry.io/4508513616920656",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
