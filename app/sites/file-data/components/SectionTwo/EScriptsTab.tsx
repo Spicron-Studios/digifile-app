a30101ccd2f161c5932c58b3ce8c622c94f44ef9
FireHawk Studio <39730717+LordFirehawk@users.noreply.github.com>
2025-03-29 12:55:36 +0200

    Added File Data

---
+++ app/sites/file-data/components/SectionTwo/EScriptsTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EScriptsTab() {
+  return (
+    <div className="p-6">
+      <h3 className="text-xl font-semibold text-gray-800">eScripts</h3>
+      <p className="mt-2 text-gray-600">eScripts functionality to be implemented.</p>
+    </div>
+  )
+}
+
---
+++ app/sites/file-data/components/SectionTwo/PaymentsTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PaymentsTab() {
+  return (
+    <div className="p-6">
+      <h3 className="text-xl font-semibold text-gray-800">Payments and Other</h3>
+      <p className="mt-2 text-gray-600">Payments and other functionality to be implemented.</p>
+    </div>
+  )
+}
+
